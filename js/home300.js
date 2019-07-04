a231c4f6f90f54e0eeeeca9ad24bb9fef4f35d2d
jiyuxia <102348056@qq.com>
2019-07-04 09:29:15 +0800

    提交成功

--- js/home300.js
+++ js/home300.js
@@ -9,9 +9,10 @@ var passName = document.querySelector(".passName");
 var passname = document.querySelector(".passname");
 var idName = document.querySelector(".idName");
 var idname = document.querySelector(".idname");
-var yzmName = document.querySelector(".yzmName");
-var yzmname = document.querySelector(".yzmname");
 var form = document.querySelector("#form");
+var dl = document.querySelector(".dl");
+var user = document.getElementsByName("user")[0];
+var pass=document.getElementsByName('pass')[0];
 
 //轮播图
 var index = 0;
@@ -97,6 +98,7 @@ uName.onblur = function(){
         uname.style.opacity = 0;
     }else{
         uname.style.opacity = 1;
+        alert("账号有误哦!~");
         userFlag = false;
     }
 }
@@ -119,6 +121,7 @@ passName.onblur = function(){
         passname.style.opacity = 0;
     } else {
         passname.style.opacity = 1;
+        alert("密码有误哦!~");
         pwdFlag = false;
     }
 }
@@ -142,31 +145,124 @@ idName.onblur = function(){
         idname.style.opacity = 0;
     } else {
         idname.style.opacity = 1;
+        alert("身份证有误哦!~");
         idFlag = false;
     }
 }
 
-//==============验证码验证=================
-/* function checkPass(){
-    var pVal = passName.value;
-    if(pVal){//true通过
-        return true;
+// --------------------显示随机验证码-------------------------
+
+$(function(){
+    var show_num = [];
+    draw(show_num);
+
+    $("#canvas").on('click',function(){
+        draw(show_num);
+    })
+    $(".dl").on('click',function(){
+        var val = $(".input-val").val().toLowerCase();
+        var num = show_num.join("");
+        if(val==''){
+            alert('请输入验证码！');
+        }else if(val == num){
+            alert('提交成功！');
+            $(".input-val").val('');
+            draw(show_num);
+
+        }else{
+            alert('验证码错误！请重新输入！');
+            $(".input-val").val('');
+            draw(show_num);
+        }
+    })
+})
+
+function draw(show_num) {
+    var canvas_width=$('#canvas').width();
+    var canvas_height=$('#canvas').height();
+    var canvas = document.getElementById("canvas");//获取到canvas的对象，演员
+    var context = canvas.getContext("2d");//获取到canvas画图的环境，演员表演的舞台
+    canvas.width = canvas_width;
+    canvas.height = canvas_height;
+    var sCode = "A,B,C,E,F,G,H,J,K,L,M,N,P,Q,R,S,T,W,X,Y,Z,1,2,3,4,5,6,7,8,9,0";
+    var aCode = sCode.split(",");
+    var aLength = aCode.length;//获取到数组的长度
+    for (var i = 0; i <= 3; i++) {
+        var j = Math.floor(Math.random() * aLength);//获取到随机的索引值
+        var deg = Math.random() * 30 * Math.PI / 180;//产生0~30之间的随机弧度
+        var txt = aCode[j];//得到随机的一个内容
+        show_num[i] = txt.toLowerCase();
+        var x = 10 + i * 20;//文字在canvas上的x坐标
+        var y = 20 + Math.random() * 8;//文字在canvas上的y坐标
+        context.font = "bold 23px 微软雅黑";
+
+        context.translate(x, y);
+        context.rotate(deg);
+
+        context.fillStyle = randomColor();
+        context.fillText(txt, 0, 0);
+
+        context.rotate(-deg);
+        context.translate(-x, -y);
     }
-    return false;//未通过
+    for (var i = 0; i <= 5; i++) { //验证码上显示线条
+        context.strokeStyle = randomColor();
+        context.beginPath();
+        context.moveTo(Math.random() * canvas_width, Math.random() * canvas_height);
+        context.lineTo(Math.random() * canvas_width, Math.random() * canvas_height);
+        context.stroke();
+    }
+    for (var i = 0; i <= 30; i++) { //验证码上显示小点
+        context.strokeStyle = randomColor();
+        context.beginPath();
+        var x = Math.random() * canvas_width;
+        var y = Math.random() * canvas_height;
+        context.moveTo(x, y);
+        context.lineTo(x + 1, y + 1);
+        context.stroke();
+    }
 }
 
-$(".yzm").click(function(){
-    $.ajax({
-        url:
-    })
-}) */
-
+function randomColor() {//得到随机的颜色值
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + "," + g + "," + b + ")";
+}
 //==============所有验证通过=================
 form.onsubmit = function(){
-    if(userFlag&&pwdFlag&&idFlag){
+    if(userFlag&&pwdFlag&&idFlag&&draw){
+        /* alert("注册成功~!"); */
         return true;
-        alert(1)
     }else{
+        alert("注册失败~!")
         return false;
     }
 }
+
+//注册账号
+dl.onclick = function(){
+    if(!user.value || !pass.value){
+        alert("账号或密码不能为空啦!~");
+        return;
+    }/* else if(checkName()&&checkPass()&&checkId()){ */
+        ajax({
+            type:"get",
+            url:"http://localhost/zheng/login2.php",
+            data:"act=add"+"&user="+user.value+"&pass="+pass.value,
+            succeed:function(str){
+                alert(str);
+                /* var json = JSON.parse(str);
+                if (json.arr) {
+                    alert(json.msg);
+                }else{
+                    alert(json.msg)
+                } */
+            },
+            failed:function(code){
+                alert(code);
+            }
+        })
+   /*  } */
+}
+
